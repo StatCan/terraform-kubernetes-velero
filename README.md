5ef3c1093accce30703c8465582790a8217216b5
Zachary Seguin <zachary.seguin@canada.ca>
2021-07-07 12:08:42 -0400

    feat: Update for terraform 0.14 and new specification

--- README.md
+++ README.md
@@ -22,7 +22,7 @@ The following security controls can be met through configuration of this templat
 
 ```terraform
 module "helm_velero" {
-  source = "git::https://github.com/canada-ca-terraform-modules/terraform-kubernetes-velero.git?ref=v3.1.1"
+  source = "git::https://github.com/canada-ca-terraform-modules/terraform-kubernetes-velero.git?ref=v3.1.0"
 
   chart_version = "2.13.6"
   dependencies = [
@@ -42,13 +42,9 @@ module "helm_velero" {
   azure_subscription_id = var.velero_azure_subscription_id
   azure_tenant_id       = var.velero_azure_tenant_id
 
-  enable_monitoring     = true
-  monitoring_namespace  = module.namespace_monitoring.name
-  servicemonitor_labels = {
-    app     = "velero"
-    release = module.helm_kube_prometheus_stack.helm_release
-  }
-  metrics_port          = 8085
+  enable_monitoring    = 1
+  monitoring_namespace = module.namespace_monitoring.name
+  metrics_port         = 8085
 
   values = <<EOF
 velero:
@@ -110,9 +106,8 @@ EOF
 | azure_resource_group          | string | yes      | The Resource Group in where the Client ID resides.            |
 | azure_subscription_id         | string | yes      | The Azure Subscription ID.                                    |
 | azure_tenant_id               | string | yes      | The Azure Tenant ID.                                          |
-| enable_monitoring             | bool   | no       | Adds metrics Service and Prometheus Operator ServiceMonitor.  |
+| enable_monitoring             | string | no       | Adds metrics Service and Prometheus Operator ServiceMonitor.  |
 | monitoring_namespace          | string | no       | The namespace where Prometheus Operator is installed.         |
-| servicemonitor_labels         | map    | no       | The labels of the Velero ServiceMonitor.                      |
 | metrics_port                  | number | no       | The service port for Prometheus metrics.                      |
 
 ## History
@@ -125,5 +120,3 @@ EOF
 | 20201013 | v2.0.1     | Add the ability to specify a username and password.                |
 | 20201013 | v3.0.0     | Remove prefix for velero subchart due to moving to upstream chart. |
 | 20201209 | v3.1.0     | Add Service and ServiceMonitor for Prometheus Operator monitoring. |
-| 20210301 | v3.1.1     | Refactor for plan noise from ServiceMonitor and deprecated syntax. |
-| 20210412 | v3.2.0     | Add servicemonitor_labels variable.                                |
--- variables.tf
+++ variables.tf
@@ -1,89 +1,55 @@
-variable "helm_namespace" {
-  description = "The namespace Helm will install the chart under"
-}
+variable "helm_namespace" {}
 
 variable "helm_repository" {
-  description = "The repository where the Helm chart is stored"
+  default = "https://vmware-tanzu.github.io/helm-charts"
 }
 variable "helm_repository_password" {
-  default     = ""
-  description = "The password of the repository where the Helm chart is stored"
+  default = ""
 }
 variable "helm_repository_username" {
-  default     = ""
-  description = "The username of the repository where the Helm chart is stored"
+  default = ""
 }
 
 variable "chart_version" {
-  description = "Version of the Helm chart"
+  default = "2.13.6"
 }
 
 # Monitoring
 
 variable "enable_monitoring" {
-  type        = bool
-  default     = false
-  description = "Adds metrics Service and Prometheus Operator ServiceMonitor"
+  type    = string
+  default = "0"
 }
 variable "monitoring_namespace" {
-  type        = string
-  default     = "monitoring"
-  description = "The namespace where Prometheus Operator is installed"
-}
-variable "servicemonitor_labels" {
-  type        = map
-  default     = {
-    app     = "velero"
-    release = "kube-prometheus-stack"
-  }
-  description = "The labels of the Velero ServiceMonitor"
+  type    = string
+  default = "monitoring"
 }
 variable "metrics_port" {
-  type        = number
-  default     = 8085
-  description = "The service port for Prometheus metrics"
+  type    = number
+  default = 8085
 }
 
 # Backup Storage Location
 
-variable "backup_storage_resource_group" {
-  description = "The resource group containing the bucket"
-}
-variable "backup_storage_account" {
-  description = "The storage account containing the bucket"
-}
-variable "backup_storage_bucket" {
-  description = "The bucket to use for backing up"
-}
+variable "backup_storage_resource_group" {}
+variable "backup_storage_account" {}
+variable "backup_storage_bucket" {}
 
 # Credentials
 
-variable "azure_client_id" {
-  description = "The Azure Client ID to use to access the storage account"
-}
+variable "azure_client_id" {}
+
 variable "azure_client_secret" {
-  description = "The Client Secret to use for the storage backend"
+  default = ""
 }
 
-variable "azure_resource_group" {
-  description = "The Resource Group in where the Client ID resides"
-}
+variable "azure_resource_group" {}
 
-variable "azure_subscription_id" {
-  description = "The Azure Subscription ID"
-}
+variable "azure_subscription_id" {}
 
-variable "azure_tenant_id" {
-  description = "The Azure Tenant ID"
-}
-
-variable "dependencies" {
-  type        = list
-  description = "Dependency name refering to namespace module"
-}
+variable "azure_tenant_id" {}
 
 variable "values" {
-  default     = ""
-  type        = string
-  description = "Values to be passed to the Helm Chart"
+  default = ""
+  type    = string
 }
