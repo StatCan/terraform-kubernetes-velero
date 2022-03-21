950b91e5f91837f23791c6dc3eaefa65cbba045a
Frances Zsurka <frances.zsurka@cloud.statcan.ca>
2022-03-21 13:48:53 +0000

    feat: kubernetes_manifest for ServiceMonitor + terraform update

--- main.tf
+++ main.tf
@@ -1,25 +1,5 @@
-# Part of a hack for module-to-module dependencies.
-# https://github.com/hashicorp/terraform/issues/1178#issuecomment-449158607
-# and
-# https://github.com/hashicorp/terraform/issues/1178#issuecomment-473091030
-# Make sure to add this null_resource.dependency_getter to the `depends_on`
-# attribute to all resource(s) that will be constructed first within this
-# module:
-resource "null_resource" "dependency_getter" {
-  triggers = {
-    my_dependencies = "${join(",", var.dependencies)}"
-  }
-
-  lifecycle {
-    ignore_changes = [
-      triggers["my_dependencies"],
-    ]
-  }
-}
-
 resource "helm_release" "velero" {
-  depends_on = [null_resource.dependency_getter]
-  name       = "velero"
+  name = "velero"
 
   repository          = var.helm_repository
   repository_username = var.helm_repository_username
@@ -64,8 +44,7 @@ EOF
 }
 
 resource "kubernetes_service" "velero" {
-  count      = var.enable_monitoring ? 1 : 0
-  depends_on = [null_resource.dependency_getter]
+  count = var.enable_monitoring ? 1 : 0
 
   metadata {
     name      = "velero-metrics"
@@ -90,64 +69,34 @@ resource "kubernetes_service" "velero" {
   }
 }
 
-resource "local_file" "velero_servicemonitor" {
-  count      = var.enable_monitoring ? 1 : 0
-  depends_on = [null_resource.dependency_getter]
-
-  sensitive_content = <<EOF
-apiVersion: monitoring.coreos.com/v1
-kind: ServiceMonitor
-metadata:
-  name: velero-monitor
-  namespace: ${var.monitoring_namespace}
-  labels: 
-    ${indent(4, yamlencode(var.servicemonitor_labels))}
-spec:
-  selector:
-    matchLabels:
-      app.kubernetes.io/name: velero
-      app.kubernetes.io/instance: velero
-  namespaceSelector:
-    matchNames:
-    - ${var.helm_namespace}
-  endpoints:
-  - port: monitoring
-EOF
-  filename          = "${path.module}/config/velero/velero-servicemonitor.yaml"
-}
-
-resource "null_resource" "apply_servicemonitor" {
+resource "kubernetes_manifest" "velero_servicemonitor" {
   count = var.enable_monitoring ? 1 : 0
-  depends_on = [
-    null_resource.dependency_getter,
-    local_file.velero_servicemonitor[0]
-  ]
-  triggers = {
-    manifest             = local_file.velero_servicemonitor[0].sensitive_content,
-    monitoring_namespace = var.monitoring_namespace
-  }
 
-  provisioner "local-exec" {
-    command = "kubectl -n ${var.monitoring_namespace} apply -f ${local_file.velero_servicemonitor[0].filename}"
+  manifest = {
+    "apiVersion" = "monitoring.coreos.com/v1"
+    "kind"       = "ServiceMonitor"
+    "metadata" = {
+      "labels"    = var.servicemonitor_labels
+      "name"      = "velero-monitor"
+      "namespace" = var.monitoring_namespace
+    }
+    "spec" = {
+      "endpoints" = [
+        {
+          "port" = "monitoring"
+        },
+      ]
+      "namespaceSelector" = {
+        "matchNames" = [
+          var.helm_namespace,
+        ]
+      }
+      "selector" = {
+        "matchLabels" = {
+          "app.kubernetes.io/instance" = "velero"
+          "app.kubernetes.io/name"     = "velero"
+        }
+      }
+    }
   }
-
-  # The "try" handles the failing edge-case of updating a pre-existing version of this null_resource that did not have the monitoring_namespace trigger.
-  # Such an update occurs as "destroy and then create replacement", but cannot proceed with a null trigger.
-  # This, along with the null resource and local file themselves, is a temporary workaround until we are able to manage CRDs in Terraform directly.
-  provisioner "local-exec" {
-    when       = destroy
-    command    = try("kubectl -n ${self.triggers.monitoring_namespace} delete servicemonitor velero-monitor", "")
-    on_failure = continue
-  }
-}
-
-# Part of a hack for module-to-module dependencies.
-# https://github.com/hashicorp/terraform/issues/1178#issuecomment-449158607
-resource "null_resource" "dependency_setter" {
-  # Part of a hack for module-to-module dependencies.
-  # https://github.com/hashicorp/terraform/issues/1178#issuecomment-449158607
-  # List resource(s) that will be constructed last within the module.
-  depends_on = [
-    helm_release.velero
-  ]
 }
